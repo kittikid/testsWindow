328e1bc25749dd38eaf38239b1b1165962e2c774
Студент <Student@College.edu>
2022-10-11 17:43:46 +0300

    init

---
+++ KursTest/Windows/Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KursTest.Windows
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AuthorizationCommit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AuthorizationRollBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+    }
+}
